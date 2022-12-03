d13ba8d6d24d5f3d826fcb49516090dde832d50b
LuukEbenau <45255779+LuukEbenau@users.noreply.github.com>
2022-12-03 22:03:00 +0100

    changed

--- stream/stream-generator/setup_kafka.py
+++ stream/stream-generator/setup_kafka.py
@@ -1,12 +1,16 @@
 from kafka.admin import KafkaAdminClient, NewTopic
-def setup_kafka():
-  def create_topics(admin, topic_list):
+
+def delete_topics(admin):
+    admin.delete_topics(topics=['chicago_crimes'])
+
+
+def create_topics(admin, topic_list):
     admin.create_topics(new_topics=topic_list, validate_only=False)
-    
-  admin_client = KafkaAdminClient(bootstrap_servers="34.27.65.215:9092",
-                                  client_id='datatengineering-group6')  # use your VM's external IP Here!
-  topic_list = [NewTopic(name="chicago_crimes", num_partitions=1, replication_factor=1), NewTopic(name="chicago_crimes_per_community", num_partitions=1, replication_factor=1)]
-  create_topics(admin_client, topic_list)
+
 
 if __name__ == '__main__':
-	setup_kafka() # sets up the topics
+    admin_client = KafkaAdminClient(bootstrap_servers="34.27.65.215:9092",
+                                    client_id='Lab8')  # use your VM's external IP Here!
+    topic_list = [NewTopic(name="chicago_crimes", num_partitions=1, replication_factor=1),
+                  NewTopic(name="chicago_crimes_per_community", num_partitions=1, replication_factor=1)]
+    create_topics(admin_client, topic_list)
